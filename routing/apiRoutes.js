ca9ea86ef8d7da0e6181739f31c1c10de5c5e951
DoDatVu <datvu22@gmail.com>
2019-04-09 18:18:20 -0500

    updated apiroutes

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -6,7 +6,22 @@ app.get("/api/home", function(req,res){
  res.json(tableData);
 });
 
+app.post("/api/tables", function(req, res) {
+
+  if (tableData.length < 10) {
+    tableData.push(req.body);
+    res.json(true);
+  }
+  else {
+    waitListData.push(req.body);
+    res.json(false);
+  }
+
+
+});
 
 
 
-})
+
+
+}
